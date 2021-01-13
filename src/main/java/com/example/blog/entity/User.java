614b85870e3f42decb17b87851eb6504851fe3f3
Angelina Leshkis <Aleshkis@mail.ru>
2021-01-13 17:22:34 +0300

    Add connection to database

--- src/main/java/com/example/blog/entity/User.java
+++ src/main/java/com/example/blog/entity/User.java
@@ -1,4 +1,110 @@
 package com.example.blog.entity;
 
+import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+import java.sql.Date;
+
+@Entity
+@Table(name = "user", catalog = "blogdb")
+@EntityListeners(AuditingEntityListener.class)
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "First name cannot be missing or empty")
+    private String firstName;
+
+    @NotBlank(message = "First name cannot be missing or empty")
+    private String lastName;
+
+    @NotBlank(message = "Password is a required field")
+    @Size(min = 8, message = "Password must be equal to or greater than 8 characters")
+    private String password;
+
+    @NotBlank(message = "Email is a required field")
+    @Pattern(regexp = "^[\\w!#$%&’*+/=?`{|}~^-]+(?:\\.[\\w!#$%&’*+/=?`{|}~^-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$")
+    private String email;
+
+    private boolean enabled;
+
+    @CreationTimestamp
+    @Column(name = "created_at")
+    private Date createdAt;
+
+    private Role role;
+
+    public User() {
+        this.enabled = false;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
 }
