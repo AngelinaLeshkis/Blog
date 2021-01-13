17874d1577b9462af31912e078aa3d9900567f14
Angelina Leshkis <Aleshkis@mail.ru>
2021-01-13 17:23:07 +0300

    Add connection to database

---
+++ src/main/java/com/example/blog/entity/Role.java
@@ -0,0 +1,19 @@
+package com.example.blog.entity;
+
+import com.fasterxml.jackson.annotation.JsonValue;
+
+public enum Role {
+    ADMIN("ROLE_ADMIN"), USER("ROLE_USER");
+
+    private String valueOfRole;
+
+    Role(String valueOfRole) {
+        this.valueOfRole = valueOfRole;
+    }
+
+    @JsonValue
+    public String getValueOfRole() {
+        return valueOfRole;
+    }
+
+}
